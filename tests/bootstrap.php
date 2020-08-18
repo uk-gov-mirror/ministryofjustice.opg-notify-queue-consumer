2e6ae4948913cc5bf015b922f5622025c3421d77
Matt S <shrwdflrst@gmail.com>
2020-08-18 18:20:12 +0100

    SW-2805 Add VCR to playback respones from Notify api instead of making actual requests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,12 +3,20 @@
 declare(strict_types=1);
 
 use Psr\Log\Test\TestLogger;
+use VCR\VCR;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // This bootstrap is shared between unit tests which don't have any env vars
 // and functional tests which do and for which we want to setup services
-if (getenv('OPG_NOTIFY_API_KEY') !== false) {
+$functionalTestSupport = getenv('OPG_NOTIFY_API_KEY') !== false;
+
+if ($functionalTestSupport) {
+    VCR::configure()->enableLibraryHooks(['stream_wrapper', 'curl']);
+    VCR::configure()->enableRequestMatchers(['method', 'url', 'host', 'query_string', 'post_fields']);
+    VCR::turnOn();
+    VCR::turnOff();
+
     $config = require_once __DIR__ . '/../src/bootstrap/config.php';
     $psrLoggerAdapter = new TestLogger();
     require_once __DIR__ . '/../src/bootstrap/services.php';
---
+++ tests/functional/NotifyQueueConsumerTest/Command/Handler/SendToNotifyHandlerTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NotifyQueueConsumerTest\Functional\Command\Handler;
+
+use League\Flysystem\FileNotFoundException;
+use League\Flysystem\Filesystem;
+use NotifyQueueConsumer\Command\Handler\SendToNotifyHandler;
+use NotifyQueueConsumer\Queue\DuplicateMessageException;
+use PHPUnit\Framework\TestCase;
+use NotifyQueueConsumer\Command\Model\SendToNotify;
+use VCR\VCR;
+
+class SendToNotifyHandlerTest extends TestCase
+{
+    private const TEST_FILE_PATH = __DIR__ . '/../../../../fixtures/sample_doc.pdf';
+    private Filesystem $filesystem;
+    private SendToNotifyHandler $handler;
+
+    public function setUp(): void
+    {
+        // These services are defined in src/bootstrap/services.php and are included in tests/bootstrap.php
+        global $filesystem, $notifyClient;
+
+        parent::setUp();
+        VCR::turnOn();
+
+        $this->filesystem = $filesystem;
+        $this->handler = new SendToNotifyHandler($this->filesystem, $notifyClient);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        VCR::turnOff();
+    }
+
+    /**
+     * @throws FileNotFoundException
+     */
+    public function testHandleSuccess(): void
+    {
+        VCR::insertCassette('test_handle_success.yml');
+        $content = file_get_contents(self::TEST_FILE_PATH);
+        $destination = basename(self::TEST_FILE_PATH);
+        $this->filesystem->put($destination, $content);
+
+
+        $command = SendToNotify::fromArray(
+            [
+                'id' => '123',
+                'uuid' => 'test-handle-success-20200818175308',
+                'filename' => $destination,
+                'documentId' => '1234',
+            ]
+        );
+
+        $updateDocumentStatus = $this->handler->handle($command);
+
+        VCR::eject();
+
+        self::assertNotEmpty($updateDocumentStatus->getNotifyId());
+        self::assertNotEmpty($updateDocumentStatus->getNotifyStatus());
+    }
+
+    /**
+     * @throws FileNotFoundException
+     */
+    public function testDuplicateUuidThrowsExceptionFailure(): void
+    {
+        self::expectException(DuplicateMessageException::class);
+
+        VCR::insertCassette('test_duplicate_uuid_throws_exception_failure.yml');
+        $content = file_get_contents(self::TEST_FILE_PATH);
+        $destination = basename(self::TEST_FILE_PATH);
+        $this->filesystem->put($destination, $content);
+
+        // Contains a uuid we sent previously
+        $command = SendToNotify::fromArray(
+            [
+                'id' => '123',
+                'uuid' => 'test-handle-success-20200818175307',
+                'filename' => $destination,
+                'documentId' => '1234',
+            ]
+        );
+
+        $this->handler->handle($command);
+
+        VCR::eject();
+    }
+}
